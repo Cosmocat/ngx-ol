4421890c89bc48f9e11637138d50935696570224
Andrey Zasorin <andrey.a.zasorin@nasa.gov>
2019-10-04 10:57:36 -0400

    target fix

--- apps/ngx-ol-demo/src/app/app.module.ts
+++ apps/ngx-ol-demo/src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxOlModule } from '@myworkspace/ngx-ol';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    NgxOlModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([])
+
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
